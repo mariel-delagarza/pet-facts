df5df390353b3d72084b399767484cee2bc33561
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-03 00:51:04 +0000

    filled out pets table file

--- db/migrate/20190403004739_create_pets_table.rb
+++ db/migrate/20190403004739_create_pets_table.rb
@@ -1,4 +1,22 @@
 class CreatePetsTable < ActiveRecord::Migration
   def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.string :breed
+      t.string :microchip_number
+      t.string :microchip_registry
+      t.string :vet_name
+      t.string :vet_phone
+      t.text :vaccinations
+      t.string :flea_treatment_type
+      t.string :heartworm_treatment_type
+      t.string :allergies
+      t.string :food_brand
+      t.string :food_serving
+      t.string :feedings_per_day
+      t.text :comments
+      t.integer :user_id
+    end
   end
 end
