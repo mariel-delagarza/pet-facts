4a90fe295d1d4f6b831882594e6aaf5665f5d525
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-03 03:11:32 +0000

    updated post /pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,6 +8,25 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
+  post '/pets' do
+    @pet = Pet.create(
+        name: params[:name],
+        species: params[:species],
+        breed: params[:breed],
+        microchip_number: params[:microchip_number],
+        microchip_registry: params[:microchip_registry],
+        vet_name: params[:vet_name],
+        vet_phone: params[:vet_phone],
+        vaccinations: params[:vaccinations],
+        flea_treatment_type: params[:flea_treatment_type],
+        heartworm_treatment_type: params[:heartworm_treatment_type],
+        allergies: params[:allergies],
+        food_brand: params[:food_brand],
+        food_serving: params[:food_serving],
+        feedings_per_day: params[:feedings_per_day],
+        comments: params[:comments])
+  end
+
   get '/pets/show' do
     erb :'/pets/show'
   end
