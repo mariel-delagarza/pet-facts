d7bbdb946ee4d04b3860c30e5d810ff5535419f3
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-03 03:50:52 +0000

    updated pets controllers

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,25 +9,35 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.new(
-        name: params[:name],
-        species: params[:species],
-        breed: params[:breed],
-        microchip_number: params[:microchip_number],
-        microchip_registry: params[:microchip_registry],
-        vet_name: params[:vet_name],
-        vet_phone: params[:vet_phone],
-        vaccinations: params[:vaccinations],
-        flea_treatment_type: params[:flea_treatment_type],
-        heartworm_treatment_type: params[:heartworm_treatment_type],
-        allergies: params[:allergies],
-        food_brand: params[:food_brand],
-        food_serving: params[:food_serving],
-        feedings_per_day: params[:feedings_per_day],
-        comments: params[:comments])
+    @pet = current_user.pets.new(
+      name: params[:name],
+      species: params[:species],
+      breed: params[:breed],
+      microchip_number: params[:microchip_number],
+      microchip_registry: params[:microchip_registry],
+      vet_name: params[:vet_name],
+      vet_phone: params[:vet_phone],
+      vaccinations: params[:vaccinations],
+      flea_treatment_type: params[:flea_treatment_type],
+      heartworm_treatment_type: params[:heartworm_treatment_type],
+      allergies: params[:allergies],
+      food_brand: params[:food_brand],
+      food_serving: params[:food_serving],
+      feedings_per_day: params[:feedings_per_day],
+      comments: params[:comments])
+    if @pet.save
+      redirect to '/pets/#{@pet.id}'
+    else
+      redirect to '/pets/new'
+    end
   end
 
   get '/pets/show' do
+    erb :'/pets/show'
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find_by_id(params[:id])
     erb :'/pets/show'
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,15 @@ class UsersController < ApplicationController
   end
 
   post '/users/create' do
-    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    @user.save
-    erb :'/pets/index'
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/pets/index'
+    else
+      redirect to '/users/create'
+    end
     #binding.pry
   end
+
 
 end
