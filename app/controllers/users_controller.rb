fa3ddfaa85dc141b3695b3d84c2fe8d093125035
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-03 01:33:04 +0000

    started setting up post /users/create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     erb :'/users/create'
   end
 
+  post '/users/create' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    @user.save
+    binding.pry
+  end
+
 end
