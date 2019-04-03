f5c861f0d3b146329de4882a02186b50c4afd425
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-03 04:06:10 +0000

    worked on login form and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
     #binding.pry
   end
 
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/pets'
+    else
+      redirect to '/login'
+    end
+  end
 
 
 
