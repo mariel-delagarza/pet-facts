fb05ad3b179150e4ce0ce55912f903c9fc32ac8b
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-05 23:55:56 +0000

    updated post/pets to redirect if not logged in

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,28 +10,32 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = current_user.pets.create(
-      name: params[:name],
-      species: params[:species],
-      breed: params[:breed],
-      microchip_number: params[:microchip_number],
-      microchip_registry: params[:microchip_registry],
-      vet_name: params[:vet_name],
-      vet_phone: params[:vet_phone],
-      vaccinations: params[:vaccinations],
-      flea_treatment_type: params[:flea_treatment_type],
-      heartworm_treatment_type: params[:heartworm_treatment_type],
-      allergies: params[:allergies],
-      food_brand: params[:food_brand],
-      food_serving: params[:food_serving],
-      feedings_per_day: params[:feedings_per_day],
-      comments: params[:comments])
-    if @pet.save
-      redirect to "/pets/#{@pet.id}"
+    if !logged_in?
+      redirect to "/"
     else
-      redirect to "/pets/new"
+      @pet = current_user.pets.create(
+        name: params[:name],
+        species: params[:species],
+        breed: params[:breed],
+        microchip_number: params[:microchip_number],
+        microchip_registry: params[:microchip_registry],
+        vet_name: params[:vet_name],
+        vet_phone: params[:vet_phone],
+        vaccinations: params[:vaccinations],
+        flea_treatment_type: params[:flea_treatment_type],
+        heartworm_treatment_type: params[:heartworm_treatment_type],
+        allergies: params[:allergies],
+        food_brand: params[:food_brand],
+        food_serving: params[:food_serving],
+        feedings_per_day: params[:feedings_per_day],
+        comments: params[:comments])
+        if @pet.save
+          redirect to "/pets/#{@pet.id}"
+        else
+          redirect to "/pets/new"
+        end
+      end
     end
-  end
 
   get '/pets/:id' do
     @pet = current_user.pets.find_by_id(params[:id])
