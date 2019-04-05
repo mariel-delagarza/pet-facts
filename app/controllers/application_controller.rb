b351dbbfb1302b932f3c17de11f7b4bca22bf609
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-05 23:50:55 +0000

    updated get  '/' controller to redirect if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect to "/pets"
+    else
+      erb :welcome
+    end
   end
 
   helpers do
